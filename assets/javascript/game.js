7786159465efdd3e1e160bfd553a090acbc8a6e0
Ryan Judy <ryan-judy@Ryans-MacBook-Pro.local>
2017-05-10 20:28:53 -0400

    fourth round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,41 @@
 var cleArray = ["terminal tower", "west side market", "playhouse square", 
 			"lebron james", "the flats", "lake erie", "detroit shoreway"]
 
-
-
 function chooseWord () {
     var randomWord = cleArray[Math.floor(Math.random() * cleArray.length)];
-	var answerArray = []
- 		for (var i = 0; i < randomWord.length; i++) {
-
-      answerArray[i] = "_";
-      document.getElementById("blank").innerHTML = answerArray;
-    }
+	var hiddenWord = "_ ".repeat(randomWord.length)
+      document.getElementById("blank").innerHTML = hiddenWord ;
+    
+    
 
 
-document.onkeyup = function guess( event ) {
-	var guess = event.key;
+document.onkeyup = function choice( event ) {
+	var choice = event.key;
 	var guesses = 8;
+	var guess = randomWord.indexOf(choice);
 
-	for (var i = 0; i < randomWord.length; i++) {
-		if( randomWord.length === guess ) {
-			answerArray.push(guess); 
+		if( guess >= 0) {
+			for (var i = 0; i < randomWord.length; i++) {
+				var ok = randomWord[i]
+			}
 
+			console.log(guess);
+			console.log(randomWord);
+			console.log(ok);
+
+
+			// hiddenWord.replace("_", correct);
+			
+		
+			document.getElementById("blank").innerHTML = choice;
+			
+			
+
+      }
 		}
 
-		else {
-			guesses--;
+		
 
 		}
-
-	return;
-	}
-
-
-};
-
-};
-
-
+		
 
